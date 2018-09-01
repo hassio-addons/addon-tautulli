3187c591d3184b69a1b8ea1526583e65b64291c8
ludeeus <joasoe@gmail.com>
2018-09-01 22:43:03 +0200

    ✨Adds butmecoffee link

---
+++ tautulli/rootfs/etc/cont-init.d/30-patches.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Tautulli
+# Patches for Tautulli
+# ==============================================================================
+
+# Adds buymeacoffe link
+patch /opt/data/interfaces/default/base.html /patches/buymeacoffe
