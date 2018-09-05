b391dad173e68be23fd3956d00a2cbcc819fb210
Joakim Sørensen <ludeeus@users.noreply.github.com>
2018-09-05 17:48:14 +0200

    ✨Adds buymecoffee link and support information (#4)
    * :white_check_mark: Added logic to check if it's the first run after an upgrade.
    
    * ✨Adds butmecoffee link
    
    * Catch up with master
    
    * Adds add-on support information

---
+++ tautulli/rootfs/etc/cont-init.d/30-patches.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# Community Hass.io Add-ons: Tautulli
+# Patches for Tautulli
+# ==============================================================================
+
+# Adds buymeacoffe link
+patch /opt/data/interfaces/default/base.html /patches/buymeacoffe
+
+# Adds add-on support information in the settings of Tautulli
+patch /opt/data/interfaces/default/base.html /patches/support
