01f529df562454dff83aab4cff7f3af152bc40ec
Franck Nijhof <frenck@frenck.nl>
2018-09-15 13:41:56 +0200

    :ambulance: Fixes incorrect patch file name (#8)

--- tautulli/rootfs/etc/cont-init.d/30-patches.sh
+++ tautulli/rootfs/etc/cont-init.d/30-patches.sh
@@ -5,7 +5,7 @@
 # ==============================================================================
 
 # Adds buymeacoffe link
-patch /opt/data/interfaces/default/base.html /patches/buymeacoffe
+patch /opt/data/interfaces/default/base.html /patches/buymeacoffee
 
 # Adds add-on support information in the settings of Tautulli
 patch /opt/data/interfaces/default/configuration_table.html /patches/support
